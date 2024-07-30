e770f2e56373adf863566fe86395012615a2054a
Omercan <115994610+Ankhies@users.noreply.github.com>
2024-07-30 16:42:08 +0300

    -

--- Stajproje/Controllers/ServiceController.cs
+++ Stajproje/Controllers/ServiceController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stajproje.Models;
 
 namespace Stajproje.Controllers
 {
     public class ServiceController : Controller
     {
-        
-        public IActionResult Service()
+        private readonly ServiceDbContext context;
+
+        public ServiceController(ServiceDbContext context)
         {
-            return View();
+            this.context = context;
+        }
+        [HttpGet]   
+        public async Task<IActionResult> Service()
+        {
+            var list = await context.Services.ToListAsync(); 
+            return View(list);
         }
     }
 }
